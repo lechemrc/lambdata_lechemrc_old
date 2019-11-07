a90a3a3005d38bd2d59293007585dd66b21d044e
Rob LeCheminant <roblecheminant@gmail.com>
2019-11-07 15:13:44 -0700

    updated df_utils.py

--- lambdata_lechemrc/df_utils.py
+++ lambdata_lechemrc/df_utils.py
@@ -6,35 +6,39 @@ rand_50 = np.random.random(50)
 rand_100 = np.random.random(100)
 
 
-# Real functionality below
-# ------------------------
+class BigData:
+	def __init__(self, dataframe):
+		self.dataframe = dataframe
 
-# Checks for null values
-def check_nulls(df):
-	return df.isna().sum()
+	def data_head(self):
+		return self.dataframe.head()
+
+	def check_nulls(df):
+		'''checks dataframe for null values'''
+		return df.isna().sum()
 
 	
-def train_test_val(df, target_val, feature_vals=df.columns.drop(target)):
-	'''returns simple train, test, and val splits with test 
-	and val equal sizes'''
+	def train_test_val(df, target_val, feature_vals=df.columns.drop(target)):
+		'''returns simple train, test, and val splits with test
+		and val equal sizes'''
 
-	# Setting target and features (with all columns - target) for baseline
-	target = target
-	features = feature_vals 
-	# dropping 'best_new_music' in order to not leak the data
+		# Setting target and features (with all columns - target) for baseline
+		target = target
+		features = feature_vals
+		# dropping 'best_new_music' in order to not leak the data
 
-	# setting my X and y
-	X = df[features]
-	y = df[target]
+		# setting my X and y
+		X = df[features]
+		y = df[target]
  
-	# Train test split
-	X_train, X_test, y_train, y_test = train_test_split(
-		X, y, test_size=0.2, random_state=42)
+		# Train test split
+		X_train, X_test, y_train, y_test = train_test_split(
+			X, y, test_size=0.2, random_state=42)
 
-	# Train val split 
-	# I chose 25% so that I would make the val and test sets the same size
-	X_train, X_val, y_train, y_val = train_test_split(
-		X_train, y_train, test_size=0.25, random_state=42)
+		# Train val split
+		# I chose 25% so that I would make the val and test sets the same size
+		X_train, X_val, y_train, y_val = train_test_split(
+			X_train, y_train, test_size=0.25, random_state=42)
 
-	return X_train.shape, y_train.shape, X_val.shape, y_val.shape, X_test.shape, y_test.shape
+		return X_train.shape, y_train.shape, X_val.shape, y_val.shape, X_test.shape, y_test.shape
 	
